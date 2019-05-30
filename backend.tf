b9e243376b0f3d4a5b534c966b76d1f4f517a06a
Donald L Mull Jr <donald@betmix.com>
2019-05-30 13:40:57 -0400

    delete unused files

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
- backend "gcs" {
-   bucket  = "pearwood-terraform-admin"
-   prefix  = "grpc-web-hello/terraform/state"
-   project = "pearwood-terraform-admin"
- }
-}
--- cluster-shell.sh
+++
@@ -1,5 +0,0 @@
-eval $(minikube docker-env)
-docker build -t my-utility-image . -f debug.Dockerfile
-kubectl delete deploy util --context minikube --namespace util
-kubectl --context minikube run -i --tty --rm --namespace util util --image=my-utility-image
-
